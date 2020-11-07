482bc68fc96de75a8aa224154e6057946645e9f3
Felix Luciano <felixluciano.a@gmail.com>
2020-11-07 16:55:06 -0500

    Added registration, login and some more templates

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template, request, url_for, redirect,session
-
+import googlemaps
 app = Flask(__name__) 
 
 @app.route("/login",methods = ["POST","GET"])  
-def home():
+def login():
     if request.method == "POST":
         for i in request.form:
             print(i,type(request.form[i]))
         return render_template("form.html")
     else:
         return render_template("form.html") 
-
+@app.route("/register",methods = ["POST","GET"])  
+def register():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        return render_template("register.html")
+    else:
+        return render_template("register.html") 
 @app.route("/")
 def mainpage():
     return render_template("index.html")
